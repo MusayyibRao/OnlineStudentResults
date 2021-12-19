e09e7d19db60dbb76d1ef8797390be1a2a589902
Musa <Musarao6396@gmail.com>
2021-12-19 18:31:15 +0530

    students results project

---
+++ OnlineStudentResults/src/java/com/Project/ConnectionProvider.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Project;
+import java.sql.*;
+
+public class ConnectionProvider {
+    
+    public static Connection getCon()
+    {
+    
+    
+    try{
+    
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","musarao");
+        
+        
+        
+        return con;
+        
+               
+    }
+    catch(Exception e)
+    {
+    e.printStackTrace();
+    
+    
+    return null;
+    
+    }
+    
+    
+    
+    }
+    
+}
